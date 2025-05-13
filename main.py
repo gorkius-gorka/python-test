fb60ccfee0ad1fad71dbe48cfdee13fd92eb1024
gorkius-gorka <gudkovivan2@gmail.com>
2025-05-13 06:47:01 +0300

    Test

--- main.py
+++
@@ -1,7 +0,0 @@
-alpha = 10
-beta = 20
-gamma = True
-if gamma and alpha < beta:
-	print(True)
-else:
-	print(False)
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
+
+print(f"{Fore.GREEN}Программа запущена{Style.RESET_ALL}")
