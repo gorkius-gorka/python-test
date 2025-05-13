1de00416eec8f8d2ed37872499ce59349080458f
gorkius <gudkovivan2@gmail.com>
2025-05-13 05:50:48 +0300

    Updating variables

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import random
 
-numbers = "0, 1, 2, 3, 4, 5, 6, 7"
-numbers = numbers.replace(" ", "")
-numbers = numbers.split(",")
+numbers_str = "0, 1, 2, 3, 4, 5, 6, 7"
+numbers_str = numbers_str.replace(" ", "")
+numbers_str = numbers_str.split(",")
 try:
-	numbers = [int(number) for number in numbers]
+	numbers = [int(number) for number in numbers_str]
 	print(numbers)
 	random_number = random.choice(numbers)
 	print(random_number)
 except ValueError:
 	print("Ошибка приведения типов")
-	
+
